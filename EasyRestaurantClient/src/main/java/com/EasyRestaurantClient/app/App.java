97b3db4fdaa0d1310f54a18e8e346970459d46a3
Carlos <carloslago@opendeusto.es>
2019-03-29 13:48:06 +0100

    Client Creation

---
+++ EasyRestaurantClient/src/main/java/com/EasyRestaurantClient/app/App.java
@@ -0,0 +1,13 @@
+package com.EasyRestaurantClient.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
