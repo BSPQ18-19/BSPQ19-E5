9fd8cda02bda8f7ed4d7d9bbd27e4a8bfe1c04dc
Carlos <carloslago@opendeusto.es>
2019-03-31 18:39:27 +0200

    Server models

--- EasyRestaurantsServer/polls/admin.py
+++ EasyRestaurantsServer/polls/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import Client, Restaurant, Reservation
 # Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('get_name',)
+    # list_filter = ['get_name']
+    search_fields = ['get_name']
+    def get_name(self, obj):
+        return obj.user.username
+    get_name.short_description = 'User'
+    # get_name.admin_order_field = 'client__user'
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location' , 'score')
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'date', 'number_clients' )
+    list_filter = ['date']
+    search_fields = ['get_name']
+    def get_name(self, obj):
+        return obj.restaurant.name
+    get_name.short_description = 'Restaurant'
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Reservation, ReservationAdmin)
--- EasyRestaurantsServer/polls/views.py
+++ EasyRestaurantsServer/polls/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from polls.forms import *
+
+@csrf_exempt
+def client_api(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        if not User.objects.filter(username=data['username']).exists():
+            new = User.objects.create()
+            new.username = data['username']
+            new.first_name = data['name']
+            new.password = data['password']
+            new.email = data['email']
+            new.save()
+
+            cl = Client.objects.create(user=new)
+            return JsonResponse("User created", safe=False)
+        else:
+            return JsonResponse("Username already exists", safe=False)
+
+
